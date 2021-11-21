3645380c5855169c9615460ff0a81c883ba8816a
unknown <samcho8@gmail.com>
2021-11-21 09:34:19 -0800

    Finished program

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-var message1 = "This is a randomly generated message";
+var goodluck = "Good luck!";
+var badluck = "Trust nobody.";
+var intermediate = "Be wary of who you trust.";
+var interest = "Try to get people to talk to you today!";
+var num = Math.floor(Math.random() * 5);
+switch (num)
+{
+    case 1:
+        console.log(goodluck);
+        break;
+    case 2:
+        console.log(intermediate);
+        break;
+    case 3:
+        console.log(interest);
+        break;
+    case 4:
+        console.log(badluck);
+        break;
+    
+}
