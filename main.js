ad24b144096251e66ab7e7e67a60bc9c2a232119
unknown <samcho8@gmail.com>
2021-11-21 09:41:09 -0800

    Made finishing touches

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ var goodluck = "Good luck!";
 var badluck = "Trust nobody.";
 var intermediate = "Be wary of who you trust.";
 var interest = "Try to get people to talk to you today!";
+var neutral = "Do what you want.";
 var num = Math.floor(Math.random() * 5);
+console.log(num);
 switch (num)
 {
+    case 0:
+        console.log(neutral);
+        break;
     case 1:
         console.log(goodluck);
         break;
